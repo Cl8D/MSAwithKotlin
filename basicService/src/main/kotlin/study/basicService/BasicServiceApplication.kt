12fb1c665835fe7414b6b9b94d85de6b6d5b4ea4
jiwon <ljwon77@daum.net>
2022-10-20 22:52:38 +0900

    Initial Setting

---
+++ basicService/src/main/kotlin/study/basicService/BasicServiceApplication.kt
@@ -0,0 +1,11 @@
+package study.basicService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BasicServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BasicServiceApplication>(*args)
+}
---
+++ basicService/src/test/kotlin/study/basicService/BasicServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.basicService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
