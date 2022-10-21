a2a68e9ed7814288eb693a6199d4a29ca5e0f397
jiwon <ljwon77@daum.net>
2022-10-22 01:34:28 +0900

    Style: 주석 제거

--- basicService/src/main/kotlin/study/basicService/CustomerApplication.kt
+++ basicService/src/main/kotlin/study/basicService/CustomerApplication.kt
@@ -14,8 +14,6 @@ class CustomerApplication {
                 Customer(3, "Hello3"))
     }
 
-    // 서로 다른 요청이 맵의 동일한 요소에 액세스 시 동기화 문제가 발생할 수 있기 때문에 concurrentHashMap 사용
-    // associateBy를 활용하면 프로퍼티를 Key-value 형태로 묶어줄 수 있다.
     @Bean
     fun customers() = ConcurrentHashMap<Long, Customer>(initialCustomers.associateBy(Customer::id))
 
