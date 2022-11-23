95d23330822a859e6484e90fba6206c2351a36ca
jiwon <ljwon77@daum.net>
2022-11-23 23:53:04 +0900

    feat: mono를 활용하여 고객 정보 response + 비어있을 경우 에러 처리 진행
    - flatMap: Mono로 반환하는 역할
    - switchIfEmpty: 비어있을 경우 처리할 행위 지정
    - ServerResponse.status() : Http 상태코드 지정
    - BodyInserters.fromValue() : 내부의 값을 가져온다고 생각.

--- reactiveService/src/main/kotlin/study/reactiveservice/handler/CustomerHandler.kt
+++ reactiveService/src/main/kotlin/study/reactiveservice/handler/CustomerHandler.kt
@@ -1,5 +1,6 @@
 package study.reactiveservice.handler
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.ServerRequest
@@ -26,5 +27,28 @@ class CustomerHandler(private val customerService: CustomerService) {
                     .body(customerService
                             .getCustomerByR(serverRequest.pathVariable("id").toLong()),
                             Customer::class.java)
-    
+
+
+    /** Mono를 활용하여 반환하도록 변경한다.*/
+    // 1. Mono<Customer>를 구독하고 있으며, getCustomerByR에 의해서 반환된다.
+    // 2. Mono<Customer>에 값이 있으면, it 내부의 Customer 객체를 받게 된다.
+    // 2-1. 값이 없다면 notFound (Http 상태 코드)를 지정한다.
+    // 3, fromValue를 통해 새로운 Mono<Customer>를 만들어서 반환받는다.
+    // 4. 생성한 Mono<Customer>를 활용하여 Mono<ServerResponse>를 생성하여 반환한다.
+    // -> 그러면, Mono<ServerResponse>를 구독하고 있다면 해당 subscriber는 Customer 객체를 받게 된다.
+    fun getCustomerByMono(serverRequest: ServerRequest) =
+            customerService.getCustomerByR(
+                    serverRequest.pathVariable("id").toLong())
+                    // flatMap을 활용하여 응답으로 변환할 Mono를 얻는다.
+                    // 이때 내부에서 Mono를 구독하고 있다가, 값이 존재하면 ok().body()를 통해 Mono<ServerResponse>를 생성한다.
+                    .flatMap {
+                        ServerResponse
+                                .ok()
+                                // fromValue를 활용하여 내부의 Customer형을 가져온다고 생각하면 된다.
+                                .body(BodyInserters.fromValue(it))
+                    }
+                    // 본문이 비어있다면, 404 리턴 후 build를 통해 응답 완료 처리. (BodyBuilder 타입이기 때문에 build로 처리가 필요하다)
+//                    .switchIfEmpty(ServerResponse.notFound().build())
+                    // 같은 내용이지만 이런 식으로 status() 메서드를 활용할 수도 있다.
+                    .switchIfEmpty(ServerResponse.status(HttpStatus.NOT_FOUND).build())
 }
