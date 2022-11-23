be35d6e2eb78f3fe46e3de3eef111307d35642b0
jiwon <ljwon77@daum.net>
2022-11-23 22:18:45 +0900

    feat: 고객에 대한 컨트롤러 추가

---
+++ reactiveService/src/main/kotlin/study/reactiveservice/controller/CustomerController.kt
@@ -0,0 +1,32 @@
+package study.reactiveservice.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import study.reactiveservice.domain.Customer
+import study.reactiveservice.service.CustomerService
+
+@RestController
+@RequestMapping("/customer")
+class CustomerController {
+
+    @Autowired
+    private lateinit var customerService: CustomerService
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: Long) : ResponseEntity<Customer> {
+        val customer = customerService.getCustomer(id)
+        return ResponseEntity(customer, HttpStatus.OK)
+    }
+
+    // http://localhost:8080/customer/search?nameFilter=micro
+    @GetMapping("/search")
+    fun searchCustomer(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
+            customerService.searchCustomer(nameFilter)
+
+}
